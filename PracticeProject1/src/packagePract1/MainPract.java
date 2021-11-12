c7c387cea06d22909ef63c759cdd4c3bfab22dea
MeghaTheQA <meghasagare7@gmail.com>
2021-11-12 18:22:23 +0530

    My first commit is done

---
+++ PracticeProject1/src/packagePract1/MainPract.java
@@ -0,0 +1,12 @@
+package packagePract1;
+
+public class MainPract {
+
+	public static void main(String[] args) 
+	{
+		PractProg1 obj = new PractProg1();
+		obj.practice1();
+
+	}
+
+}
---
+++ PracticeProject1/src/packagePract1/PractProg1.java
@@ -0,0 +1,10 @@
+package packagePract1;
+
+public class PractProg1
+{
+	public void practice1()
+	{
+		System.out.println("Hi, I am practicing git");
+	}
+
+}
